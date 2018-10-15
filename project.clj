27e5d80b10404270b5d57cb524b210b9ceb6a876
Michel Perez <michel.ingesoft@gmail.com>
2018-10-14 22:06:47 -0500

    wip

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defprojet rea-inc "0.1.0-SNAPSHOT"
+(defproject rea-inc "0.1.0-SNAPSHOT"
   :description "FIXME: write this!"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+            :url  "http://www.eclipse.org/legal/epl-v10.html"}
 
   :min-lein-version "2.7.1"
 
@@ -10,7 +10,7 @@
 
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/clojurescript "1.9.908"]
-                 [org.clojure/core.async  "0.3.443"]
+                 [org.clojure/core.async "0.3.443"]
                  [cljs-http "0.1.44"]
                  [reagent "0.7.0"]
                  [funcool/cuerdas "2.0.5"]]
@@ -22,92 +22,92 @@
   :source-paths ["src"]
 
   :cljsbuild {:builds
-              [{:id "dev"
+              [{:id           "dev"
                 :source-paths ["src"]
 
                 ;; The presence of a :figwheel configuration here
                 ;; will cause figwheel to inject the figwheel client
                 ;; into your build
-                :figwheel {:on-jsload "rea-inc.core/on-js-reload"
-                           ;; :open-urls will pop open your application
-                           ;; in the default browser once Figwheel has
-                           ;; started and compiled your application.
-                           ;; Comment this out once it no longer serves you.
-                           :open-urls ["http://localhost:3449/index.html"]}
+                :figwheel     {:on-jsload "rea-inc.core/on-js-reload"
+                               ;; :open-urls will pop open your application
+                               ;; in the default browser once Figwheel has
+                               ;; started and compiled your application.
+                               ;; Comment this out once it no longer serves you.
+                               :open-urls ["http://localhost:3449/index.html"]}
 
-                :compiler {:main rea-inc.core
-                           :asset-path "js/compiled/out"
-                           :output-to "resources/public/js/compiled/rea_inc.js"
-                           :output-dir "resources/public/js/compiled/out"
-                           :source-map-timestamp true
-                           ;; To console.log CLJS data-structures make sure you enable devtools in Chrome
-                           ;; https://github.com/binaryage/cljs-devtools
-                           :foreign-libs [{:file "js/demo.js"
-                                           :provides ["ext.demo"]
-                                           :module-type :commonjs}]
+                :compiler     {:main                 rea-inc.core
+                               :asset-path           "js/compiled/out"
+                               :output-to            "resources/public/js/compiled/rea_inc.js"
+                               :output-dir           "resources/public/js/compiled/out"
+                               :source-map-timestamp true
+                               ;; To console.log CLJS data-structures make sure you enable devtools in Chrome
+                               ;; https://github.com/binaryage/cljs-devtools
+                               :foreign-libs         [{:file        "js/demo.js"
+                                                       :provides    ["ext.demo"]
+                                                       :module-type :commonjs}]
 
-                           :preloads [devtools.preload]}}
+                               :preloads             [devtools.preload]}}
                ;; This next build is a compressed minified build for
                ;; production. You can build this with:
                ;; lein cljsbuild once min
-               {:id "min"
+               {:id           "min"
                 :source-paths ["src"]
-                :compiler {:output-to "resources/public/js/compiled/rea_inc.js"
-                           :main rea-inc.core
-                           :optimizations :advanced
-                           :pretty-print false}}]}
+                :compiler     {:output-to     "resources/public/js/compiled/rea_inc.js"
+                               :main          rea-inc.core
+                               :optimizations :advanced
+                               :pretty-print  false}}]}
 
   :figwheel {;; :http-server-root "public" ;; default and assumes "resources"
-             :server-port 3449 ;; default
-             :server-ip "127.0.0.1"
+             :server-port 3449                              ;; default
+             :server-ip   "127.0.0.1"
 
-             :css-dirs ["resources/public/css"] ;; watch and update CSS
+             :css-dirs    ["resources/public/css"]          ;; watch and update CSS
 
              ;; Start an nREPL server into the running figwheel process
-             :nrepl-port 7888}
+             :nrepl-port  7888}
 
-             ;; Server Ring Handler (optional)
-             ;; if you want to embed a ring handler into the figwheel http-kit
-             ;; server, this is for simple ring servers, if this
+  ;; Server Ring Handler (optional)
+  ;; if you want to embed a ring handler into the figwheel http-kit
+  ;; server, this is for simple ring servers, if this
 
-             ;; doesn't work for you just run your own server :) (see lein-ring)
+  ;; doesn't work for you just run your own server :) (see lein-ring)
 
-             ;; :ring-handler hello_world.server/handler
+  ;; :ring-handler hello_world.server/handler
 
-             ;; To be able to open files in your editor from the heads up display
-             ;; you will need to put a script on your path.
-             ;; that script will have to take a file path and a line number
-             ;; ie. in  ~/bin/myfile-opener
-             ;; #! /bin/sh
-             ;; emacsclient -n +$2 $1
-             ;;
-             ;; :open-file-command "myfile-opener"
+  ;; To be able to open files in your editor from the heads up display
+  ;; you will need to put a script on your path.
+  ;; that script will have to take a file path and a line number
+  ;; ie. in  ~/bin/myfile-opener
+  ;; #! /bin/sh
+  ;; emacsclient -n +$2 $1
+  ;;
+  ;; :open-file-command "myfile-opener"
 
-             ;; if you are using emacsclient you can just use
-             ;; :open-file-command "emacsclient"
+  ;; if you are using emacsclient you can just use
+  ;; :open-file-command "emacsclient"
 
-             ;; if you want to disable the REPL
-             ;; :repl false
+  ;; if you want to disable the REPL
+  ;; :repl false
 
-             ;; to configure a different figwheel logfile path
-             ;; :server-logfile "tmp/logs/figwheel-logfile.log"
+  ;; to configure a different figwheel logfile path
+  ;; :server-logfile "tmp/logs/figwheel-logfile.log"
 
-             ;; to pipe all the output to the repl
-             ;; :server-logfile false
+  ;; to pipe all the output to the repl
+  ;; :server-logfile false
 
 
 
   ;; Setting up nREPL for Figwheel and ClojureScript dev
   ;; Please see:
   ;; https://github.com/bhauman/lein-figwheel/wiki/Using-the-Figwheel-REPL-within-NRepl
-  :profiles {:dev {:dependencies [[binaryage/devtools "0.9.4"]
-                                  [figwheel-sidecar "0.5.16"]
-                                  [cider/piggieback "0.3.8"]]
+  :profiles {:dev {:dependencies  [[binaryage/devtools "0.9.4"]
+                                   [figwheel-sidecar "0.5.16"]
+                                   [cider/piggieback "0.3.8"]]
                    ;; need to add dev source path here to get user.clj loaded
-                   :source-paths ["src" "dev"]
+                   :source-paths  ["src" "dev"]
                    ;; for CIDER
-                   :plugins [[cider/cider-nrepl "0.15.1"]]
-                   :repl-options {:nrepl-middleware [cider.piggieback/wrap-cljs-repl]}
+                   :plugins       [[cider/cider-nrepl "0.15.1"]]
+                   :repl-options  {:nrepl-middleware [cider.piggieback/wrap-cljs-repl]}
                    ;; need to add the compliled assets to the :clean-targets
                    :clean-targets ^{:protect false} ["resources/public/js/compiled"
                                                      :target-path]}})
